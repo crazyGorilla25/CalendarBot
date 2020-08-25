2a56c871e1826eab83d43f12cc30124fe7140a85
Schuyler Henry <schenry823@gmail.com>
2020-08-25 16:16:25 -0700

    wrggr

--- CalendarBot/Program.cs
+++ CalendarBot/Program.cs
@@ -15,46 +15,25 @@ namespace CalendarBot
     class Program
     {
         private DiscordSocketClient client;
-        ISocketMessageChannel channelToAnnounce;
         
-        static void Main(string[] args)
+        static void Main()
         {
             new Program().MainAsync().GetAwaiter().GetResult();
         }
         
-        public async Task MainAsync() {
+        async Task MainAsync() {
             client = new DiscordSocketClient();
 
             client.Log += Log;
 
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-            var token = "NzQwNjYxNjAwMTg4NDk4MTAx.XysQ3g.GX7_U6pYzDq3u1UVkTCqv5LXtq";
-
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            var token = "NzQwNjYxNjAwMTg4NDk4MTAx.XysQ3g.GX7_U6pYzDq3u1UVkTCqv5LXtq0";
 
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
-
-            client.MessageReceived += MessageRecieved;
             await AnnounceEvents();
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
         }
 
-        private async Task MessageRecieved(SocketMessage message) {
-            if(message.Content == "*setChannelAnnounce")
-            {
-                channelToAnnounce = message.Channel;
-                await message.Channel.SendMessageAsync("Announce channel set.");
-            }
-        }
-
-        private Task Log(LogMessage msg)
+        Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
@@ -62,8 +41,10 @@ namespace CalendarBot
 
         async Task AnnounceEvents()
         {
+            Console.WriteLine("Announcing events");
             DateTime now = DateTime.Now;
-            DateTime timeToRun = new DateTime(now.Year, now.Month, now.Day, 7, 0, 0);
+            DateTime timeToRun = new DateTime(now.Year, now.Month, now.Day, 8, 0, 0);
+            //DateTime timeToRun = now.AddSeconds(10);
             TimeSpan delay;
             if(timeToRun > now)
             {
@@ -71,7 +52,7 @@ namespace CalendarBot
             }
             else
             {
-                timeToRun.AddDays(1);
+                timeToRun = timeToRun.AddDays(1);
                 delay = timeToRun - now;
             }
 
@@ -82,18 +63,46 @@ namespace CalendarBot
             string calString = Encoding.UTF8.GetString(calBytes);
             Calendar pHSCalendar = Calendar.Load(calString);
             List<CalendarEvent> todayEvents = new List<CalendarEvent>();
+            Console.WriteLine(DateTime.Today);
             for (int i = 0; i < pHSCalendar.Events.Count; i++)
             {
                 CalendarEvent eventInQuestion = pHSCalendar.Events[i];
-                if (eventInQuestion.Start.Date == DateTime.Today)
+                if ((eventInQuestion.Start.Date <= DateTime.Today && DateTime.Today < eventInQuestion.End.Date) || eventInQuestion.Start.Date == DateTime.Today)
                 {
                     todayEvents.Add(eventInQuestion);
+                    Console.WriteLine(eventInQuestion.Summary + " Start: " + eventInQuestion.Start.Date + " End: "  + eventInQuestion.End.Date);
                 }
             }
-            if (channelToAnnounce != null)
+
+            SocketGuild server = client.GetGuild(634944914756730880);
+            ISocketMessageChannel channel = server.GetTextChannel(634945249864581150);
+
+            SocketRole role = server.GetRole(740738668255379546);
+            string announcementMessage = role.Mention + "\nToday's events are:\n";
+            if (todayEvents.Count == 0)
             {
-                await channelToAnnounce.SendMessageAsync(todayEvents[0].Summary);
+                announcementMessage = role.Mention + "\nThere are no events listed today.";
             }
+            else if (todayEvents.Count == 1)
+            {
+                announcementMessage += "**" + todayEvents[0].Summary + "**";
+            }
+            else
+            {
+                for (int i = 0; i < todayEvents.Count; i++)
+                {
+                    if (i != todayEvents.Count - 1)
+                    {
+                        announcementMessage += "**" + todayEvents[i].Summary + "**, \n\n";
+                    }
+                    else
+                    {
+                        announcementMessage += "and **" + todayEvents[i].Summary + "**";
+                    }
+                }
+            }
+
+            await channel.SendMessageAsync(announcementMessage);
         }
     }
 }
---
+++ Run.sh
@@ -0,0 +1,2 @@
+cd /Users/schuylerhenry/Desktop/Code/C#/CalendarBot/CalendarBot/bin/Release/netcoreapp3.1/publish
+dotnet CalendarBot.dll
