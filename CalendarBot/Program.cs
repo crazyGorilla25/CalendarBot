b2312ccf66d6f050536c393343c1e9529259b64a
Schuyler Henry <schenry823@gmail.com>
2021-03-03 23:00:04 -0800

    Added attendance link and focus program mentions

--- CalendarBot/Program.cs
+++ CalendarBot/Program.cs
@@ -1,23 +1,31 @@
-﻿using System;
-using System.Text;
-using System.Net;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
+﻿using Discord;
+using Discord.WebSocket;
 using Ical.Net;
 using Ical.Net.CalendarComponents;
-
-using Discord;
-using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CalendarBot
 {
-    class Program
+	class Program
     {
-        private DiscordSocketClient client;
+        readonly string techFocus = "TFP";
+        readonly string medFocus = "MFP";
+        readonly string cinemaFocus = "CFP";
+
+        readonly ulong tfpId = 747307858676416544, mfpId = 747307883653496862, cfpId = 747307901940531310;
+        DiscordSocketClient client;
+        static bool normalTime = false;
         
         static void Main()
         {
+            Console.WriteLine("Run at normal time? Y or N");
+            char answer = Console.ReadKey().KeyChar;
+            normalTime = answer == 'y' || answer == 'Y';
+            Console.Write("\n");
             new Program().MainAsync().GetAwaiter().GetResult();
         }
         
@@ -43,33 +51,38 @@ namespace CalendarBot
         {
             Console.WriteLine("Announcing events");
             DateTime now = DateTime.Now;
-            DateTime timeToRun = new DateTime(now.Year, now.Month, now.Day, 8, 0, 0);
-            //DateTime timeToRun = now.AddSeconds(5);
-            TimeSpan delay;
-            if(timeToRun > now)
+            DateTime timeToRun;
+            if (normalTime)
             {
-                delay = timeToRun - now;
+                timeToRun = new DateTime(now.Year, now.Month, now.Day, 8, 0, 0);
             }
             else
             {
-                timeToRun = timeToRun.AddDays(1);
-                delay = timeToRun - now;
+                timeToRun = now.AddSeconds(5);
             }
+            TimeSpan delay;
+			if (timeToRun <= now)
+			{
+                timeToRun = timeToRun.AddDays(1);
+			}
+            delay = timeToRun - now;
 
-            await Task.Delay(delay);
+            Console.WriteLine(delay.Hours + " hours, " + delay.Minutes + " minutes, & " + delay.Seconds + " seconds");
 
             WebClient webClient = new WebClient();
             byte[] calBytes = webClient.DownloadData("https://www.providencehigh.org/master/calendar/feed/ical.ics");
             string calString = Encoding.UTF8.GetString(calBytes);
             Calendar pHSCalendar = Calendar.Load(calString);
             List<CalendarEvent> todayEvents = new List<CalendarEvent>();
+
             Console.WriteLine(DateTime.Today);
+
             for (int i = 0; i < pHSCalendar.Events.Count; i++)
             {
 
                 CalendarEvent eventInQuestion = pHSCalendar.Events[i];
-                Console.WriteLine(eventInQuestion.Summary + " Start: " + eventInQuestion.Start.Date + " End: " + eventInQuestion.End.Date);
-                if ((eventInQuestion.Start.Date <= DateTime.Today && DateTime.Today < eventInQuestion.End.Date) || eventInQuestion.Start.Date == DateTime.Today.Date)
+                //Console.WriteLine(eventInQuestion.Summary + " Start: " + eventInQuestion.Start.ToTimeZone("America/Los_Angeles").Date + " End: " + eventInQuestion.End.ToTimeZone("America/Los_Angeles").Date);
+                if (EventIsToday(eventInQuestion))
                 {
                     todayEvents.Add(eventInQuestion);
                 }
@@ -79,6 +92,10 @@ namespace CalendarBot
             ISocketMessageChannel channel = server.GetTextChannel(634945249864581150);
 
             SocketRole role = server.GetRole(740738668255379546);
+            SocketRole tfpRole = server.GetRole(tfpId);
+            SocketRole mfpRole = server.GetRole(mfpId);
+            SocketRole cfpRole = server.GetRole(cfpId);
+
             string announcementMessage = role.Mention + "\nToday's events are:\n";
             Console.WriteLine(todayEvents.Count);
             if (todayEvents.Count == 0)
@@ -93,18 +110,37 @@ namespace CalendarBot
             {
                 for (int i = 0; i < todayEvents.Count; i++)
                 {
+                    string summary = todayEvents[i].Summary;
+                    summary = summary.Replace(techFocus, tfpRole.Mention);
+                    summary = summary.Replace(medFocus, mfpRole.Mention);
+                    summary = summary.Replace(cinemaFocus, cfpRole.Mention);
+
                     if (i != todayEvents.Count - 1)
                     {
-                        announcementMessage += "**" + todayEvents[i].Summary + "**, \n\n";
+                        announcementMessage += "**" + summary + "**, \n\n";
                     }
                     else
                     {
-                        announcementMessage += "and **" + todayEvents[i].Summary + "**";
+                        announcementMessage += "and **" + summary + "**";
                     }
                 }
             }
 
+            announcementMessage += "\n\nSubmit your attendance here: https://bit.ly/3b9YQ0L";
+
+            Console.WriteLine(announcementMessage);
+
+
+            await Task.Delay(delay);
+
             await channel.SendMessageAsync(announcementMessage);
+        }
+
+        bool EventIsToday(CalendarEvent eventInQuestion)
+		{
+            return (eventInQuestion.Start.ToTimeZone("America/Los_Angeles").Date <= DateTime.Today && DateTime.Today < eventInQuestion.End.ToTimeZone("America/Los_Angeles").Date) ||
+                    eventInQuestion.Start.ToTimeZone("America/Los_Angeles").Date == DateTime.Today.Date.ToLocalTime();
+
         }
     }
 }
